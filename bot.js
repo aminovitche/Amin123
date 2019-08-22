3abac7acef29dbeff499be0f2037d7ed485fa858
aminovitche <54403043+aminovitche@users.noreply.github.com>
2019-08-22 14:39:52 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("614075248144285701")
+setInterval(function() {
+channel.send(`Hi all spam no`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
